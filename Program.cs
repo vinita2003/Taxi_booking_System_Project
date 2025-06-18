306553bd3fb2797ada96636d0d1de960249892f2
Vinita Shah <vinita.shah@intimetec.com>
2025-06-18 21:23:24 +0530

    changeFolderStructure

--- Program.cs
+++ Program.cs
@@ -1,52 +1,84 @@
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
-using Taxi_Booking_System;
-using Taxi_Booking_System.Interface;
-using Taxi_Booking_System.Middleware;
-using Taxi_Booking_System.MappingProfile;
-using Taxi_Booking_System.Repository;
-using Taxi_Booking_System.Services;
+using TaxiBooking.Api.Middleware;
+using TaxiBooking.Application.Interface;
+using TaxiBooking.Application.Services;
+using TaxiBooking.Infastructure.Data;
+using TaxiBooking.Infastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TaxiBooking.RealTime.Hubs;
+using TaxiBooking.Application.Interfaces;
+
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
-builder.Logging.AddDebug();
-builder.Services.AddControllers().AddJsonOptions(options =>
+        builder.Logging.AddConsole();
+        builder.Logging.AddDebug();
+        builder.Services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policy =>
+            {
+                policy.WithOrigins("http://localhost:4200")
+                      .AllowAnyHeader()
+                      .AllowAnyMethod()
+                       .AllowCredentials();
+
+            });
+        });
+        builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        builder.Services.AddDbContext<TaxiBookingDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("MyTaxiBookingDb")));
+        builder.Services.AddScoped<IAuthServices, AuthServices>();
+builder.Services.AddScoped<IRiderServices, RiderService>();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<IDriverServices, DriverServices>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
+.AddJwtBearer(options =>
+
 {
-    options.JsonSerializerOptions.PropertyNamingPolicy = null;
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy( policy =>
+    options.TokenValidationParameters = new TokenValidationParameters
     {
-        policy.WithOrigins("http://localhost:4200")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = config["Jwt:Issuer"],
+        ValidAudience = config["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(config["Jwt:Token"]))
+    };
 });
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-builder.Services.AddDbContext<TaxiBookingDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MyTaxiBookingDb")));
-builder.Services.AddScoped<IAuthServices, AuthServices>();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddSignalR();
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-app.UseCors(); 
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseCors();
 
-app.UseMiddleware<ErrorHandlerMiddleware>();
+        app.UseMiddleware<ErrorHandlerMiddleware>();
 
-app.UseHttpsRedirection();
+        app.UseHttpsRedirection();
+app.UseCors("AllowFrontend");
+app.UseAuthentication();
 
-app.UseAuthorization();
+        app.UseAuthorization();
 
-app.MapControllers();
+        app.MapControllers();
+app.MapHub<DriverHub>("/driverHub");
 
 app.Run();
+   
